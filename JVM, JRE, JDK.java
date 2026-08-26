081701b9dd37ce9110e0c919772ff9b1c6959268
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-08-26 21:27:45 +0530

    Document JVM, JRE, and JDK functionalities
    Added explanations for JVM, JRE, and JDK, including their roles and components.

---
+++ JVM, JRE, JDK.java
@@ -0,0 +1,26 @@
+1.JVM - Java Virtual Machine
+  JVM runs Java bytecode.
+   . Converts bytecode(.class) -> machine code
+   . Makes Java platform-independent.
+   . JVM is different for each operating system.
+  Example:
+   Hello.java -> Hello.class -> JVM -> Machine Code 
+
+2.  JRE - Java Runtime Environment
+ JRE provides the environment needed to run Java programs. 
+  JRE = JVM + java Libraries
+  It contains: 
+  . JVM
+  . Java class libraries
+  . Supporting files
+JRE can run Java programs, but cannot normally be used to develop/compile them. 
+
+3.   JDK - Java Development Kit
+
+  JDk is used to develop and run Java programs. 
+
+  It contains:
+  .JVM
+  .JRE
+  .javac compiler
+  .Debugging and other development tools.
