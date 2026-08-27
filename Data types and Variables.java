e38c9b078ae64354b92330b2410c16db0f30f837
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-08-27 12:29:18 +0530

    Create Data types and Variables.java with explanations
    Added a Java file explaining data types and variables.

---
+++ Data types and Variables.java
@@ -0,0 +1,39 @@
+//Data Types in java
+
+Java data types are divided into two categories:
+  A.Primitive Data Types
+
+  These are built-in types that store simple values.
+
+  Data Type            Size             
+  1.byte               1 byte
+  2.short              2 bytes
+  3.int                4 bytes
+  4.long               8 bytes
+  5.float              4 bytes
+  6.double             8 bytes
+  7.char               2 bytes
+  8.boolean            1 bit(logical) 
+
+  B.Non-Primitive (Reference ) Data Types
+
+  These store references (addresses) of objects.
+  Examples:
+   . String
+   . Arrays
+   . Classes
+   . Interfaces
+   . Objects
+
+  2. Variables in Java
+ A variable is a named storage location that holds a value.
+
+  //Syntax
+
+  dataType variableName = value;
+Example :
+
+int age = 22;
+String name = "Akash";
+
+3.Types of Variables 
