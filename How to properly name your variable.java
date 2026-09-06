43ceb16c19509defa40998dfc4a8bab1631c9c39
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-09-06 18:30:11 +0530

    Create Java class for variable naming guidelines
    Added a Java class demonstrating variable naming conventions.

---
+++ How to properly name your variable.java
@@ -0,0 +1,13 @@
+Public class Name{
+
+  public static void main(String[] args){
+    //case sensitive 
+    // can be Letters, Digits, Dollar Signs, or Underscores
+    // Must Begin with a Letter, Dollar Sign ($), or Underscores
+    //cannot use Java keywords as variable names
+    //camel case
+    String myFullname = "Akash Bhagat" ;
+    System.out.println(myFullName);
+  }
+}
+  
