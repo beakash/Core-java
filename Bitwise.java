a99afe6b98271322d7472347cd4308bfce34b11b
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-09-07 17:55:25 +0530

    Document bitwise operators in Bitwise.java
    Added explanations and examples for bitwise operators in Java.

---
+++ Bitwise.java
@@ -0,0 +1,34 @@
+Integer.toBinaryString : this keyword is use to show binary values of integer 
+
+  //we can use bitwise operators on Operands( byte, short, int , long )
+
+  OPerators : . and operator &
+              . or operator |
+              . xor ^ 
+              . not ~ 
+              . left shift <<
+              . right shift >>
+              . unsigned right shift >>> 
+
+  //and operator & : means multiple of of binary values
+
+  ex : 
+      int c = 5 & 4 ;
+      System.out.println(c) ;
+output : 4 // & operator take binary values of 5 , 4 and multiple them and print the integer of that binary value.
+
+//Or operator means | : means if any bit is 1 then it will give 1 .
+
+  ex : 
+   int c = 5 & 7 ;
+   System.out.println(c) ;
+
+output : 7 
+
+  //xor ^   : means same bit gives 0 and different bit gives 1 .
+
+  ex:  int c = 5 & 7 ;
+   System.out.println(c) ; 
+
+output : 2
+  
