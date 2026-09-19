a120c451d5e997b41b30979da17875e0f0f5ed21
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-09-19 21:38:00 +0530

    Document array usage in Arrays.java
    Added comments and example usage for array declaration and indexing in Java.

---
+++ Arrays .java
@@ -0,0 +1,17 @@
+Arrays : stores elements of same data types in a contiguous memory allocation.
+  //Syntax : datatype[] name ;
+  //eg : int[] age ;
+  Declaration: 
+  int[] rollNums = new int[3] ;
+//int[] : datatype of array
+//rollNums : identifiers name of array 
+//new : special keyword = Allocate memory in heap.
+//int[3] : size of array .
+
+indexing : starts from 0
+  //eg : rollnums[0] = 101 ;
+  //.    rollnums[1] = 102 ;
+  //.    rollnums[2] = 102 ; 
+  System.out.println(rollnums[0]) ; //101
+  System.out.println(rollnums[1]) ; //102
+  System.out.println(rollnums[2]) ; //103
